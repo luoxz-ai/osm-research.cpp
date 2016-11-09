b543753607fa20bfa0c51b20fb77a16ea7cd94ba
Kazuki Takise <kazuki.takise@gmail.com>
2016-11-09 13:13:47 +0900

    Generalize visualizer so that it can color some nodes and edges

--- test/GraphTest.cpp
+++ test/GraphTest.cpp
@@ -2,10 +2,20 @@
 #include <string>
 #include <cstdlib>
 #include "../src/Graph.cpp"
+#include "../src/Common.cpp"
 
 using namespace std;
 
 int main(void) {
   Graph g("kyoto.in");
-  g.visualize();
+  map<int, int> cns;
+  map<pair<int, int>, int> ces;
+  double d;
+  vector<Edge> path;
+  int s = 435 % 1000, t = 5;
+  tie(d, path) = Common::Dijkstra(g, s, t);
+  cout << d << endl;
+  cns[s] = 0, cns[t] = 2;
+  for (auto e : path) ces[pair<int, int>(e.from, e.to)] = 1;
+  g.Visualize("out.png", cns, ces);
 }
